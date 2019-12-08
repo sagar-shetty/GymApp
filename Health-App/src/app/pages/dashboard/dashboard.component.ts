95b85a542c49cff902e5eab0a1cbdf71d0626eb2
sagar_shetty89 <saghar.shetty89@gmail.com>
2019-12-08 19:58:42 +0530

    Committing initial code

---
+++ Health-App/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
+
+
+@Component({
+    selector: 'dashboard-cmp',
+    moduleId: module.id,
+    templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent implements OnInit{
+
+
+    ngOnInit(){
+
+    }
+}
---
+++ Health-App/src/app/shared/fixedplugin/fixedplugin.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'fixedplugin-cmp',
+    templateUrl: 'fixedplugin.component.html',
+    styleUrls: ['./fixedplugin.componenet.css']
+})
+
+export class FixedPluginComponent implements OnInit{
+
+  ngOnInit(){}
+}
