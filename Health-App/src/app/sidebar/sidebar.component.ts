db32f32c346b05ba0afcd5efcc7d3ee82942c738
sagar_shetty89 <saghar.shetty89@gmail.com>
2020-01-11 15:14:07 +0530

    Committing updates to the application

--- Health-App/src/app/sidebar/sidebar.component.ts
+++ Health-App/src/app/sidebar/sidebar.component.ts
@@ -11,7 +11,7 @@ export interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard',     title: 'Home',         icon:'nc-bank',       class: '' },
     { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
-    { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
+    { path: '/maps',          title: 'Our Location',              icon:'nc-pin-3',      class: '' },
     { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
     { path: '/user',          title: 'User Profile',      icon:'nc-single-02',  class: '' },
     { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
